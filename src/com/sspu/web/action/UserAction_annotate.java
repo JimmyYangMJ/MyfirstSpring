dda4df679377d38d1af3a74a9d521ae7915a764e
YMj1998 <2237895205@qq.com>
2019-07-30 20:30:31 +0800

    Spring 4

--- src/com/sspu/web/action/UserAction_annotate.java
+++ src/com/sspu/web/action/UserAction_annotate.java
@@ -4,9 +4,13 @@ import com.sspu.model.User;
 import com.sspu.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import javax.annotation.Resource;
+
 @Controller
+@Scope("prototype") //  多例
 public class UserAction_annotate {
 
     @Autowired
@@ -15,6 +19,7 @@ public class UserAction_annotate {
      *  @Service("myUserService")注解中的id，避免错误的调用了其他实现类
     */
     @Qualifier("myUserService")
+    //@Resource(name = "myUserService")//等效于前面两行代码【用的比较少】
     private IUserService userService;
 
     public void save(User user){
